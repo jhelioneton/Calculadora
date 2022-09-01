fa030e7f7adc5a3a75d18c257f6c1bc972fd3e21
Helio <jhelioneto@hotmail.com>
2022-09-01 09:54:34 -0300

    Adicionado a função dos números aparecerem na tela da calc

--- script.js
+++ script.js
@@ -1,19 +1,48 @@
-const previousOperationText = document.querySelector("#prev-operation")
-const currentOperationText = document.querySelector("#current-operation")
-const button = document.querySelectorAll("#bttn-container button")
+const previousOperationText = document.querySelector("#prev-operation");
+const currentOperationText = document.querySelector("#current-operation");
+const button = document.querySelectorAll("#bttn-container button");
 
 class Calculator {
+    constructor(previousOperationText, currentOperationText) {
+        this.previousOperationText = previousOperationText;
+        this.currentOperationText = currentOperationText;
+        this.currentOperation = "";
+    }
+    //Adiciona os números na tela.
+    addDigit(digit) {
 
+        this.currentOperation = digit
+        this.updateScreen()
+    }
+    // Muda os valores na tela da calculadora.
+    updateScreen() {
+        this.currentOperationText.innerText += this.currentOperation; //Adiciona os números da operação atual dentro do texto da operação atual. 
+    }
 }
+
+const calc = new Calculator(previousOperationText, currentOperationText);
 
 button.forEach((btn) => {
     btn.addEventListener("click", (e) => {
         const value = e.target.innerText;
 
-        if(+value >=0 || value === ".") {
-            console.log(value);
+        if (+value >= 0 || value === ".") {
+            calc.addDigit(value);
         } else {
             console.log("Op: " + value);
         }
     });
 });
+
+//Mostra no console os botões dos números e das operações sendo apertadas.
+//button.forEach((btn) => {
+   // btn.addEventListener("click", (e) => {
+       // const value = e.target.innerText;
+
+       // if(+value >=0 || value === ".") {
+           // console.log(value);
+       // } else {
+           // console.log("Op: " + value);
+      //  }
+   // });
+// });
